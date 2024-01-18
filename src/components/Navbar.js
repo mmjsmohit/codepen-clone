74056b084aa2a9e248a3ba99029dc8866aa6ed6e
mmjsmohit <mmjsmohit@gmail.com>
2024-01-18 08:36:31 +0530

    initial working commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="main lg:flex md:flex flex-wrap justify-between items-center px-4 bg-[#2f3640] py-4">
+      <div className="left">
+        <div className="logo font-bold text-2xl text-white text-center">
+          <img
+            className=" w-44"
+            src="https://blog.codepen.io/wp-content/uploads/2022/01/codepen-wordmark-display-inside-white@10x.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/components/Result.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Result({ srcCode }) {
+  return (
+    <div>
+      {srcCode ===
+      `<html>
+            <body></body>
+            <style></style>
+            <script></script>
+    </html>` ? (
+        <div className="bg-[#282c34] p-4 shadow mt-4 rounded-lg">
+          <h2 className="text-lg font-semibold mb-2 text-white text-center">
+            Result
+          </h2>
+          <iframe
+            className="w-full h-60 border border-gray-700 rounded-md"
+            srcDoc={srcCode}
+            title="output"
+            sandbox="allow-scripts"
+            width="100%"
+            height="100%"
+          ></iframe>
+        </div>
+      ) : (
+        <div className="w-full h-60 bg-[#282c34] p-4 shadow mt-4 rounded-lg">
+          <h2 className="text-lg font-semibold mb-2 text-white text-center">
+            Start Hacking!
+          </h2>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Result;
